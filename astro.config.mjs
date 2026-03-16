083cf66884c90e79646c84137a39c0c8e2da5204
mghidini <92437378+mghidini@users.noreply.github.com>
2026-03-16 23:12:11 +0100

    config and github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+//export default defineConfig({});
+
+export default defineConfig({
+  site: 'https://mghidini.github.io',
+  base: '/updated_portfolio',  // omit this if it's your user/org site
+});
